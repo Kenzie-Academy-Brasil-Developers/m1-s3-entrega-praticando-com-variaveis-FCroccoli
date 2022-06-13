8467e306f1ad482ec68933a023a2174268921c27
Fernando Croccoli <nandique@gmail.com>
2022-06-13 12:51:47 -0300

    scritp completo

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "Fernando";
+let sobrenome = "Croccoli";
+let anoNascimento = 1993;
+let cidade = "Porto Alegre";
+let idade = 2022 - anoNascimento;
+
+console.log (`Eu sou ${nome} ${sobrenome} de ${cidade} e tenho ${idade} anos`)
